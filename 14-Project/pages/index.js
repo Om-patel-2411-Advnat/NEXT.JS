078715e226acadac69e9a858d9bacb27f893329d
Om Patel <om@advant.xyz>
2026-04-10 09:55:27 +0530

    change metadata

--- 14-Project/pages/index.js
+++ 14-Project/pages/index.js
@@ -7,7 +7,7 @@ export default function HomePage(props){
     return(
         <>
             <Head>
-                <title>Max' Blog</title>
+                <title>Om' Blog</title>
                 <meta
                     name='description'
                     content='I post about programming and web development.'
